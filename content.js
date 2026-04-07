ec4326fcd464596a877476a581cc16221d15f3b2
Ian Vu <ianv6207@mytusd.org>
2026-04-07 11:38:37 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
-document.body.innerHTML
+document.body.innerHTML = "";
+document.head.innerHTML = ""; //Add icon and title later.
 const game = document.createElement("iframe");
 game.srcdoc = null;
 game.style.position = "fixed";
@@ -7,3 +8,4 @@ game.style.top = "0px";
 game.style.left = "0px";
 game.style.width = "100%";
 game.style.height = "100%";
+document.body.appendChild(game);
