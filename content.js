0c30de602e81e0b6d3d1c45c45a4107881a60fad
Ian Vu <ianv6207@mytusd.org>
2026-04-07 11:35:29 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
+document.body.innerHTML
 const game = document.createElement("iframe");
 game.srcdoc = null;
 game.style.position = "fixed";
+game.style.zIndex = 99999999;
+game.style.top = "0px";
+game.style.left = "0px";
+game.style.width = "100%";
+game.style.height = "100%";
